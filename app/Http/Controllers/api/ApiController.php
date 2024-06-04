dae33bf8cf6170f440e6a94351f75f0978b65e8b
Oltion-Koceku <koceku17@gmail.com>
2024-06-04 18:02:12 +0200

    [add] route vue and route laravel detail

--- app/Http/Controllers/api/ApiController.php
+++ app/Http/Controllers/api/ApiController.php
@@ -3,9 +3,43 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
+use App\Models\Technologie;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    public function index(){
+
+        $projects = Project::with('type', 'technologie')->paginate(20);
+
+        return response()->json($projects);
+
+    }
+
+    public function getType(){
+
+        $typs = Type::all();
+        return response()->json($typs);
+
+    }
+
+    public function getTechnologie(){
+
+        $technologies = Technologie::all();
+        return response()->json($technologies);
+
+    }
+
+    public function getDetailbySlug($slug){
+
+
+        $detail = Project::where('slug', $slug)->first();
+
+
+        return response()->json($detail);
+
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/projects', [ApiController::class, 'index']);
+Route::get('/types', [ApiController::class, 'getType']);
+Route::get('/technologies', [ApiController::class, 'getTechnologie']);
+Route::get('/get-detail-slug/{slug}', [ApiController::class, 'getDetailbySlug']);
