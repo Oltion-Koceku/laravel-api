407d0a3f4341867f982d49561854bc98c5c0f872
Oltion-Koceku <koceku17@gmail.com>
2024-06-05 16:25:23 +0200

    [add] fill and save form in db

--- app/Http/Controllers/api/LeadController.php
+++ app/Http/Controllers/api/LeadController.php
@@ -3,22 +3,58 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\FormEmail;
+use GrahamCampbell\ResultType\Success;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 
+
 class LeadController extends Controller
 {
-    public function store(Request $request){
+    public function store(Request $request)
+    {
 
         $data = $request->all();
 
-        $validator = Validator::make($data,
-        [
-            "name" => "requ"
-        ]);
+        $validator = Validator::make(
+            $data,
+            [
+                "name" => "required|min:3|max:50",
+                "email" => "required|email",
+                "message" => "required|min:10",
+            ],
+            [
+                "name.required" => "Il nome è Obbligatiorio",
+                "name.min" => "Il nome deve contenere almeno :min caratteri",
+                "name.max" => "Il nome non può avere più di :max caratteri",
 
-        return response()->json($data);
+                "email.required" => "L'email è obbligatorio",
+                "email.email" => "E' richiesta una mail Valida",
+
+                "message.required" => "Il messaggio è obbligatorio",
+                "message.min" => "Il messaggio deve contenere almeno :min caratteri",
+            ]
+        );
+
+        if ($validator->fails()) {
+            $success = false;
+            $errors = $validator->errors();
+            return response()->json(compact('success', 'errors'));
+        }
+
+
+        $new_lead = new FormEmail;
+
+        $new_lead->fill($data);
+        $new_lead->save();
+
+
+        $success = true;
+
+
+        return response()->json(compact('success'));
+
 
     }
 }
